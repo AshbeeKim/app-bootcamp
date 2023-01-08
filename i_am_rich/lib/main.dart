56dbec9c3414d5635e24f47183521b05207b129a
Ashbee <ksb.forest@gmail.com>
2023-01-08 18:04:51 +0900

    study: section3. I Am Rich - How to Create Flutter Apps From Scratch

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Text('I Am Rich'),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: Center(
+            child: Image(
+              image: NetworkImage('https://images.unsplash.com/photo-1471879832106-c7ab9e0cee23?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3773&q=80'),
+            ),
+          ),
+        ),
+      ),
+    );
